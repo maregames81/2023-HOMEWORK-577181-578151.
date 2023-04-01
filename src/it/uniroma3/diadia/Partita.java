036776b1101ff317cc54dacb60935ddf65a0ecf5
maregames81 <108728904+maregames81@users.noreply.github.com>
2023-04-01 19:16:29 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/Partita.java
@@ -0,0 +1,73 @@
+package it.uniroma3.diadia;
+
+import it.uniroma3.diadia.ambienti.Labirinto;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.giocatore.Giocatore;
+
+/**
+ * Questa classe modella una partita del gioco
+ *
+ * @author  docente di POO
+ * @see Stanza
+ * @version base
+ */
+
+public class Partita {
+
+
+	private Giocatore player;
+	private Labirinto mappa;
+	private Stanza stanzaCorrente;
+	private boolean finita;
+	
+
+
+	public Partita(){
+		this.mappa= new Labirinto();
+		this.player= new Giocatore();
+		this.stanzaCorrente=mappa.getStanzaIniziale();
+		this.finita = false;
+		
+
+	}
+
+
+	public Giocatore getPlayer() {
+		return player;
+	}
+
+	public void setStanzaCorrente(Stanza stanzaCorrente) {
+		this.stanzaCorrente = stanzaCorrente;
+	}
+
+	public Stanza getStanzaCorrente() {
+		return this.stanzaCorrente;
+	}
+
+	/**
+	 * Restituisce vero se e solo se la partita e' stata vinta
+	 * @return vero se partita vinta
+	 */
+	public boolean vinta() {
+		return this.getStanzaCorrente()== mappa.getStanzaVincente();
+	}
+
+	/**
+	 * Restituisce vero se e solo se la partita e' finita
+	 * @return vero se partita finita
+	 */
+	public boolean isFinita() {
+		return finita || vinta() || (player.getCfu() == 0);
+	}
+
+	/**
+	 * Imposta la partita come finita
+	 *
+	 */
+	public void setFinita() {
+		this.finita = true;
+	}
+
+
+
+}
---
+++ src/it/uniroma3/diadia/giocatore/Borsa.java
@@ -0,0 +1,102 @@
+package it.uniroma3.diadia.giocatore;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+public class Borsa {
+
+	public final static int DEFAULT_PESO_MAX_BORSA = 10;
+	private Attrezzo[] attrezzi;
+	private int numeroAttrezzi;
+	private int pesoMax;
+
+	public Borsa() {
+		this(DEFAULT_PESO_MAX_BORSA);
+	}
+
+	public Borsa(int pesoMax) {
+		this.pesoMax = pesoMax;
+		this.attrezzi = new Attrezzo[10]; // speriamo bastino...
+		this.numeroAttrezzi = 0;
+	}
+
+	public boolean addAttrezzo(Attrezzo attrezzo) {
+		if (this.getPeso() + attrezzo.getPeso() > this.getPesoMax())
+			return false;
+		if (this.numeroAttrezzi==10)
+			return false;
+		this.attrezzi[this.numeroAttrezzi] = attrezzo;
+		this.numeroAttrezzi++;
+		return true;
+	}
+
+	public int getPesoMax() {
+		return pesoMax;
+	}
+
+	public static int getDefaultPesoMaxBorsa() {
+		return DEFAULT_PESO_MAX_BORSA;
+	}
+
+	public int getNumeroAttrezzi() {
+		return numeroAttrezzi;
+	}
+
+	public Attrezzo getAttrezzo(String nomeAttrezzo) {
+		Attrezzo a = null;
+		for (int i= 0; i<this.numeroAttrezzi; i++)
+			if (this.attrezzi[i].getNome().equals(nomeAttrezzo))
+				a = attrezzi[i];
+
+		return a;
+	}
+
+	public int getPeso() {
+		int peso = 0;
+		for (int i= 0; i<this.numeroAttrezzi; i++)
+			peso += this.attrezzi[i].getPeso();
+
+		return peso;
+	}
+
+	public boolean isEmpty() {
+		return this.numeroAttrezzi == 0;
+	}
+
+	public boolean hasAttrezzo(String nomeAttrezzo) {
+		return this.getAttrezzo(nomeAttrezzo)!=null;
+	}
+
+	public boolean removeAttrezzo(String nomeAttrezzo) {
+		for (int i= 0; i<this.numeroAttrezzi; i++) {
+			if (this.attrezzi[i].getNome().equals(nomeAttrezzo)) {
+				if(i==this.numeroAttrezzi) {
+					attrezzi[i]=null;
+					this.numeroAttrezzi--;
+					return true;
+				} else {
+					for(int k=i+1;k<this.numeroAttrezzi;k++) {
+						attrezzi[i]=attrezzi[k];
+						i++;
+					}
+					attrezzi[i]=null;
+					this.numeroAttrezzi--;
+					return true;
+				}
+			}
+		}
+		return true;
+	}
+
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+
+		if (!this.isEmpty()) {
+			s.append("Contenuto borsa ("+this.getPeso()+"kg/"+this.getPesoMax()+"kg): ");
+			for (int i= 0; i<this.numeroAttrezzi; i++)
+				s.append(attrezzi[i].toString()+" ");
+		}
+		else
+			s.append("Borsa vuota");
+		return s.toString();
+	}
+}
